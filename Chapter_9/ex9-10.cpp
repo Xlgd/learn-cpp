ffd0f9f29f1be4e58263a24816d6568562000a74
Gaodong <1120496618@qq.com>
2020-04-11 17:09:55 +0800

    update

---
+++ Chapter_9/ex9-10.cpp
@@ -0,0 +1,11 @@
+// 练习9.10
+/* 下面4个对象分别是什么类型？*/
+
+/*
+vector<int> v1;
+const vector<int> v2;
+
+auto it1 = v1.begin(); // vector<int>::iterator
+auto it2 = v2.begin(); // vector<int>::const_iterator
+auto it3 = v1.cbegin();  // vector<int>::const_iterator
+auto it4 = v2.cbegin(); // vector<int>::const_iterator
---
+++ Chapter_9/ex9-8.cpp
@@ -0,0 +1,6 @@
+// 练习9.8
+/* 为了读取string的list中的元素，应该使用什么类型？
+如果写入list，又应该使用什么类型？*/
+
+// list<string>::const_iterator // 读
+// list<string>::iterator // 写
