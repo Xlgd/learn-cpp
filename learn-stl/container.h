377e247c5a5321d0f03266099a1e86e52ccf763c
Gaodong <lgd1120496618@gmail.com>
2020-10-11 16:14:21 +0800

    update

--- learn-stl/container.h
+++ learn-stl/container.h
@@ -1,5 +1,103 @@
 // 5.1 simple container
+# include <iostream>
+
 template <class T, size_t N>
 struct block {
     typedef T value_type;
+    typedef value_type* pointer;
+    typedef const value_type* const_pointer;
+    typedef value_type& reference;
+    typedef const value_type& const_reference;
+
+    typedef ptrdiff_t difference_type;
+    typedef size_t size_type;
+
+    typedef pointer iterator;
+    typedef const_pointer const_iterator;
+
+    iterator begin() { return data; }
+    iterator end() { return data + N; }
+
+    const_iterator begin() const { return data; }
+    const_iterator end() const { return data + N; }
+
+    typedef reverse_iterator<const_iterator> const_reverse_iterator;
+    typedef reverse_iterator<iterator> reverse_iterator;
+
+    const_reverse_iterator rbegin() const {
+        return const_reverse_iterator(end());
+    }
+
+    const_reverse_iterator rend() const {
+        return const_reverse_iterator(begin());
+    }
+
+    reference operator[](size_type n) { return data[n]; }
+    const_reference operator[](size_type n) const { return data[n]; }
+
+    size_type size() const { return N; }
+    size_type max_size() const { return N; }
+    bool empty() const { return N == 0; }
+
+    void swap(block& x) {
+        for (size_t n = 0; n < N; ++n) {
+            std::swap(data[n], x.data[n]);
+        }
+    }
+
+    T data[N];
+};
+
+template <class T, size_t N>
+bool operator==(const block<T, N>&x, const block<T, N>& y) {
+    for (size_t n = 0; n < N; ++n) {
+        if (x.data[n] != y.data[n]) {
+            return false;
+        }
+    }
+    return true;
 }
+
+template <class T, size_t N>
+bool operator<(const block<T, N>&x, const block<T, N>& y) {
+    for (size_t n = 0; n < N; ++n) {
+        if (x.data[n] < y.data[n]) {
+            return true;
+        }
+        else if (y.data[n] > x.data[n]) {
+            return false;
+        }
+    }
+    return false;
+}
+
+// 5.2 a trivial container
+//- Use it to test generic algorithm
+//- Use it as a basis to write other new STL containers.
+template <class T>
+struct trivial_container {
+    typedef T value_type;
+
+    typedef value_type* pointer;
+    typedef const value_type* const_pointer;
+    typedef value_type& reference;
+    typedef const value_type& const_reference;
+
+    typedef value_type* iterator;
+    typedef const value_type* const_iterator;
+
+    typedef ptrdiff_t difference_type;
+    typedef size_t size_type;
+
+    const_iterator begin() const { return 0; }
+    const_iterator end() const { return 0; }
+
+    iterator begin() { return 0; }
+    iterator end() { return 0; }
+
+    size_type size() const { return 0; }
+    size_type max_size() const { return 0; }
+    bool empty() const { return true; }
+
+    void swap(trivial_container&) {}
+};
